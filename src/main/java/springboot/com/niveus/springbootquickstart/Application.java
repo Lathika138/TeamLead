ce264f937e94edbca48ea85e44b9feef72cf3196
Lathika138 <lathika.k@niveussolutions.com>
2022-10-11 18:16:26 +0530

    your commit message

--- src/main/java/springboot/com/niveus/springbootquickstart/Application.java
+++ src/main/java/springboot/com/niveus/springbootquickstart/Application.java
@@ -10,7 +10,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println();
+		System.out.println("Happy to hear from you");
 	}
 
 }
